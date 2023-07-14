5309c30e99b62ba4cefd9eeaea280568897ad3c9
ROTl24 <1808395202@qq.com>
2023-07-14 16:08:49 +0800

    element主题色更改

--- vite.config.js
+++ vite.config.js
@@ -14,12 +14,25 @@ export default defineConfig({
       resolvers: [ElementPlusResolver()],
     }),
     Components({
-      resolvers: [ElementPlusResolver()],
+      //1.配置element-plus采用sass样式配色系统
+      resolvers: [ElementPlusResolver({
+        importStyle: "sass"
+      })],
     }),
   ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        // 2.自动导入定制化样式文件进行样式覆盖
+        additionalData: `
+          @use "@/styles/element/index.scss" as *;
+        `,
+      }
     }
   }
 })
