58329e94500c955564e61cb5fbf423f2d8509b72
ROTl24 <1808395202@qq.com>
2023-07-14 15:43:14 +0800

    完成别名联想设置

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+//这个配置项只做联想提示
+import HelloWorld from '@/components/HelloWorld.vue'
 </script>
 
 <template>
