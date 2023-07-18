d7b4ce93a51835b7df71eb37f5b774bdb76d378a
ROTl24 <1808395202@qq.com>
2023-07-18 15:03:06 +0800

    一级分类---面包屑导航渲染

--- src/views/Category/index.vue
+++ src/views/Category/index.vue
@@ -1,3 +1,116 @@
+<script setup>
+import { getCategoryAPI } from '@/apis/category'
+import { ref, onMounted } from 'vue'
+import { useRoute } from 'vue-router'
+// 获取数据列表
+const categoryData = ref({})
+const route = useRoute()
+const getCategory = async () => {
+    //获取了个路由参数
+    //这里的params.id要与category路由中的参数名称一致
+  const res = await getCategoryAPI(route.params.id)
+  categoryData.value = res.result
+}
+ 
+onMounted(() => {
+  getCategory()
+})
+</script>
+
 <template>
-    我是分类页
+  <div class="top-category">
+    <div class="container m-top-20">
+      <!-- 面包屑 -->
+      <div class="bread-container">
+        <!-- 提供面包屑分隔符为：> -->
+        <el-breadcrumb separator=">">
+            <!-- 提供面包屑导航项 -->
+          <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
+          <el-breadcrumb-item id="name">{{categoryData.name}}</el-breadcrumb-item>
+        </el-breadcrumb>
+      </div>
+
+    </div>
+  </div>
 </template>
+
+
+<style scoped lang="scss">
+.top-category {
+  h3 {
+    font-size: 28px;
+    color: #666;
+    font-weight: normal;
+    text-align: center;
+    line-height: 100px;
+  }
+
+  .sub-list {
+    margin-top: 20px;
+    background-color: #fff;
+
+    ul {
+      display: flex;
+      padding: 0 32px;
+      flex-wrap: wrap;
+
+      li {
+        width: 168px;
+        height: 160px;
+
+
+        a {
+          text-align: center;
+          display: block;
+          font-size: 16px;
+
+          img {
+            width: 100px;
+            height: 100px;
+          }
+
+          p {
+            line-height: 40px;
+          }
+
+          &:hover {
+            color: $xtxColor;
+          }
+        }
+      }
+    }
+  }
+
+  .ref-goods {
+    background-color: #fff;
+    margin-top: 20px;
+    position: relative;
+
+    .head {
+      .xtx-more {
+        position: absolute;
+        top: 20px;
+        right: 20px;
+      }
+
+      .tag {
+        text-align: center;
+        color: #999;
+        font-size: 20px;
+        position: relative;
+        top: -20px;
+      }
+    }
+
+    .body {
+      display: flex;
+      justify-content: space-around;
+      padding: 0 40px 30px;
+    }
+  }
+
+  .bread-container {
+    padding: 25px 0;
+  }
+}
+</style>
